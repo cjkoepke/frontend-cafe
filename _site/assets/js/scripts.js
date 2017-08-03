b6a9e2e76e0548396be85566b661560534cc7785
Calvin Koepke <hello@calvinkoepke.com>
2017-08-03 12:25:33 -0600

    Initial design.

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -1,3 +1,22 @@
-(function() {
-    console.log('hi');
-})()
+'use strict';
+
+(function () {
+
+    var error = document.getElementById('message');
+    var form = document.getElementById('sign-up');
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        if (!name.value || !email.value) displayMessage("It appears that you forgot a field! Please try again.");
+    });
+
+    var displayMessage = function displayMessage(message) {
+        error.textContent = message;
+        error.classList.add('visible');
+        setTimeout(function () {
+            error.classList.remove('visible');
+        }, 3000);
+    };
+})();
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,24 @@
+---
+---
 (function() {
-    console.log('hi');
+
+    const error = document.getElementById('message');
+    const form = document.getElementById('sign-up');
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (!name.value || !email.value)
+            displayMessage("It appears that you forgot a field! Please try again.");
+    })
+
+    const displayMessage = (message) => {
+        error.textContent = message;
+        error.classList.add('visible');
+        setTimeout(() => {
+            error.classList.remove('visible');
+        }, 3000);
+    }
+
 })()
