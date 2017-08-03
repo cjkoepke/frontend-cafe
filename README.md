b74e2ebceabf5f860127c2d4996d97e6f8873244
Calvin Koepke <hello@calvinkoepke.com>
2017-08-03 07:53:07 -0600

    replace with jekyll since this is so simple.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Frontend.Cafe
-The source code for the Frontend Cafe website. Built on Next.js.
--- components/Wrap.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Wrap = styled.div`
-    margin: 0 auto;
-    max-width: 1000px;
-    width: 100%;
-`
-
-export default Wrap;
